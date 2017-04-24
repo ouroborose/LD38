edb02a5b7b6012b7379678f1fbc5e85623084d02
stregawolf <stregawolf@gmail.com>
2017-04-23 23:11:46 -0700

    Stub for audio manager

---
+++ Assets/LD38/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager> {
+    public void PlayOneShot(AudioClip clip)
+    {
+        GameObject obj = new GameObject(clip.name);
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
+        Destroy(obj, clip.length);
+    }
+}
